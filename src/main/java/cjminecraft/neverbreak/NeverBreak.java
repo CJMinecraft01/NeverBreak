aaca5a2dd0b5d1756f027d59df915768a66f4fea
CJMinecraft01 <speed2pc2@gmail.com>
2020-07-20 11:59:21 +0100

    Fix a crash

--- src/main/java/cjminecraft/neverbreak/NeverBreak.java
+++ src/main/java/cjminecraft/neverbreak/NeverBreak.java
@@ -2,6 +2,8 @@ package cjminecraft.neverbreak;
 
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,18 +13,18 @@ public class NeverBreak {
     public static final String MODID = "neverbreak";
     public static final Logger LOGGER = LogManager.getFormatterLogger(MODID);
 
-    public static final EnchantmentType ALL = EnchantmentType.create("ALL", item -> {
-        for (EnchantmentType type : EnchantmentType.values())
-            if (type.canEnchantItem(item))
-                return true;
-        return false;
-    });
+    public static EnchantmentType ALL;
 
     public static NeverBreakEnchantment NEVER_BREAK;
 
     public NeverBreak() {
+        ALL = EnchantmentType.create("ALL", item -> {
+            for (EnchantmentType type : EnchantmentType.values())
+                if (type != ALL && type.canEnchantItem(item))
+                    return true;
+            return false;
+        });
         ForgeRegistries.ENCHANTMENTS.register(NEVER_BREAK = new NeverBreakEnchantment());
     }
-
 
 }
